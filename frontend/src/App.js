fa111e106bb0d5ee737a3160bd3078a26540404b
mrjain <mrjain@ncsu.edu>
2022-08-06 17:11:28 -0400

    updating frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
+
+  const [todoList, setTodoList] = useState([{}]);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  // Read All ToDo's
+  useEffect(() => {
+    axios.get("http://localhost:8000/api/todo")
+      .then(res => {
+        setTodoList(res.data)
+      })
+  });
+
+  // Post a Todo
+  const addTodoHandler = () => {
+    axios.post('http://localhost:8000/api/todo/', { 'title': title, 'description': description })
+      .then(res => console.log(res))
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App list-group-item justify-content-center align-items-center mx-auto" style={{"width": "400px", "backgroundColor": "white", "marginTop": "15px"}}>
+        <h1 className="card text-white bg-primary mb-1" styleName="max-width: 20rem;">Task Manager</h1>
+        <h6 className="card text-white bg-primary mb-3">FastAPI - React - MongoDB</h6>
+        <div className="card-body">
+          <h5 className="card text-white bg-dark mb-3">Add Your Task</h5>
+          <span className="card-text">
+            <input className="mb-2 form-control titleIn" onChange={event => setTitle(event.target.value)} placeholder="Title"/>
+            <input className="mb-2 form-control descriptionIn" onChange={event => setDescription(event.target.value)} placeholder="Description"/>
+            <button className="btn btn-outline-primary mx-2 mb-3" style={{"borderRadius": "50px", "font-weight": "bold"}} onClick={addTodoHandler}>Add Task</button>
+          </span>
+          <h5 className="card text-white bg-dark mb-3">Your Tasks</h5>
+          <div>
+            {/* Todo Items - external component */}
+          </div>
+        </div>
+        <h6 className="card text-dark bg-warning py-1 mb-0">Copyright 2022, All rights reserved &copy;</h6>
+      </div>
   );
 }
 
